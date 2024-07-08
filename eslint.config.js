5630a11e70cb25c450bccb48b9b644d717f358b4
itaober <itaober@gmail.com>
2024-07-08 13:46:42 +0800

    chore: remove unused code

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,3 @@
 import eslint from '@itaober/eslint-config';
 
-export default eslint({
-  react: {
-    overrides: {
-      'react/no-danger': 'warn',
-      'react/self-closing-comp': 'error',
-    },
-  },
-});
+export default eslint();
